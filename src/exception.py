e8ead9124e9c7fe4b9b3b877c85a79f9e6e26d83
Sambhav Singhal <sambhav.8761@gmail.com>
2026-09-20 11:49:21 +0530

    Connect exception to logger

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-
+from logger import logging
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
     filename=exc_tb.tb_frame.f_code.co_filename
@@ -13,3 +13,9 @@ class CustomException(Exception):
         self.error_message=error_message_detail(error_message,error_detail)
     def __str__(self):
         return self.error_message
+if __name__=='__main__':
+    try:
+        a=1/0
+    except Exception as exp:
+        logging.error('Division by Zero error')
+        raise CustomException(exp,sys)
